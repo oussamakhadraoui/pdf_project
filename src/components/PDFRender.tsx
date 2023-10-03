2f287fda2a4f013ddb794c6c6091b588e2eee1f9
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-10-03 22:40:10 +0100

    pdf implementation

--- src/components/PDFRender.tsx
+++ src/components/PDFRender.tsx
@@ -1,17 +1,157 @@
-import React from 'react'
+'use client'
+import { ChevronDown, ChevronUp, Loader2, Search } from 'lucide-react'
+import React, { useState } from 'react'
+import { Document, Page, pdfjs } from 'react-pdf'
+import 'react-pdf/dist/Page/AnnotationLayer.css'
+import 'react-pdf/dist/Page/TextLayer.css'
+import { toast } from './ui/use-toast'
+import { useResizeDetector } from 'react-resize-detector'
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+import { useForm } from 'react-hook-form'
+import z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { cn } from '@/lib/utils'
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 interface PDFRenderProps {
-  
+  url: string
 }
 
-const PDFRender = ({}: PDFRenderProps) => {
-  return <div className='w-full bg-white shadow rounded-md flex flex-col items-center'>
-   <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
-    <div className='flex items-center gap-1.5'>
-     top bar
+const PDFRender = ({ url }: PDFRenderProps) => {
+  const [numPages, setNumPage] = useState<number>()
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [scale, setScale] = useState<number>(1)
+  const { ref, width } = useResizeDetector()
+  const Validator = z.object({
+    page: z
+      .string()
+      .refine((num) => Number(num) > 0 && Number(num) <= numPages!),
+  })
+  type ValidatorForm = z.infer<typeof Validator>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<ValidatorForm>({
+    defaultValues: { page: '1' },
+    resolver: zodResolver(Validator),
+  })
+
+  const handlePageSubmit = ({ page }: ValidatorForm) => {
+    setCurrentPage(Number(page))
+    setValue('page', String(page))
+  }
+  return (
+    <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
+      <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
+        <div className='flex items-center gap-1.5'>
+          <Button
+            variant='ghost'
+            aria-label='previous page'
+            disabled={currentPage <= 1}
+            onClick={() => {
+              setCurrentPage((prev) => (prev - 1 > 1 ? prev - 1 : 1))
+            }}
+          >
+            <ChevronDown className='h-4 w-4' />
+          </Button>
+          <div className='flex items-center gap-1.5'>
+            <Input
+              {...register('page')}
+              className={cn(
+                'w-12 h-8 focus-visible:ring-white focus-visible:ring-2',
+                errors.page && 'focus-visible:ring-red-500'
+              )}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleSubmit(handlePageSubmit)()
+                }
+              }}
+            />
+            <p className='text-zinc-700 text-sm space-x-1'>
+              <span>/</span>
+              <span>{numPages ?? ''}</span>
+            </p>
+          </div>
+          <Button
+            variant='ghost'
+            aria-label='previous page'
+            disabled={numPages === undefined || currentPage === numPages}
+            onClick={() => {
+              setCurrentPage((prev) =>
+                prev + 1 > numPages! ? numPages! : prev + 1
+              )
+            }}
+          >
+            <ChevronUp className='h-4 w-4' />
+          </Button>
+        </div>
+        <div className='space-x-2'>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className='gap-1.5' aria-label='zoom' variant='ghost'>
+                <Search className='h-4 w-4' />
+                {scale * 100}%
+                <ChevronDown className='h-3 w-3 opacity-50' />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem onSelect={() => setScale(1)}>
+                100%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(1.5)}>
+                150%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(2)}>
+                200%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(2.5)}>
+                250%
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+
+      <div className='flex-1 w-full max-h-screen '>
+        <div ref={ref}>
+          <Document
+          
+            loading={
+              <div className='flex justify-center'>
+                <Loader2 className='my-24 h-6 w-6 animate-spin' />
+              </div>
+            }
+            onLoadSuccess={({ numPages }) => {
+              setNumPage(numPages)
+            }}
+            onLoadError={() => {
+              toast({
+                type: 'error',
+                variant: 'destructive',
+                message: 'Error loading PDF please try again later.',
+                title: 'Error Loading pdf!',
+              })
+            }}
+            className='max-h-full'
+            file={url}
+            
+          >
+            <Page width={width ? width : 1} pageNumber={currentPage} scale={scale} />
+          </Document>
+        </div>
+      </div>
     </div>
-   </div>
-  </div>
+  )
 }
 
 export default PDFRender
