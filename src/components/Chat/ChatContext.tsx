fc21003604f49afb519258ce5534a4c81c5feded
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-10-04 21:33:44 +0100

    chat context

---
+++ src/components/Chat/ChatContext.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface ChatContextProps {
+  
+}
+
+const ChatContext = ({}: ChatContextProps) => {
+  return <div>ChatContext</div>
+}
+
+export default ChatContext
