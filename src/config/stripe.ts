b1ffa4f6557153e51bb83af680c7c654ab6aa0df
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-10-07 23:14:28 +0100

    stripe implementation

---
+++ src/config/stripe.ts
@@ -0,0 +1,19 @@
+export const PLANS = [
+  {
+    name: 'Free',
+    slug: 'free',
+    quota: 10,
+    pagesPerPdf: 5,
+    price: { amount: 0, priceIds: { test: '', production: '' } },
+  },
+  {
+    name: 'Pro',
+    slug: 'pro',
+    quota: 50,
+    pagesPerPdf: 25,
+    price: {
+      amount: 14,
+      priceIds: { test: 'price_1NyhssE4PH0W5SI33ubww9tR', production: '' },
+    },
+  },
+]
