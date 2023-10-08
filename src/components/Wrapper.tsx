71d8e795a057cc96e87124052cafc6d6d0952115
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-10-08 21:28:26 +0100

    wrapper component

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -6,10 +6,12 @@ interface WrapperProps {
   className?: string
 }
 
+//wrapper component
 const Wrapper = ({ children, className }: WrapperProps) => {
   return (
     <div className={cn(`w-full mx-auto max-w-screen-xl px-2.5 md:px-20`,className)}>
       {children}
+
     </div>
   )
 }
